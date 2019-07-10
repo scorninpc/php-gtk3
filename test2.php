58ba99a1ee3a2844dd822d36c2e430ab5806cd94
Bruno Pitteli Gonçalves <scorninpc@gmail.com>
2019-07-10 17:27:38 -0300

    Add test2.php support for gtk 3.24

--- test2.php
+++ test2.php
@@ -15,45 +15,38 @@ class Application
 		$paned->set_position(120);
 
 		$pixbuf = GdkPixbuf::new_from_file_at_size("./logo.png", 15, -1);
-
 		// Treeview
 		$tree = new GtkTreeView();
 			$renderer = new GtkCellRendererPixbuf();
 			$column = new GtkTreeViewColumn("", $renderer, "pixbuf", 0);
 			$tree->append_column($column);
-
 			$renderer = new GtkCellRendererText();
 			$column = new GtkTreeViewColumn("", $renderer, "text", 1);
 			$tree->append_column($column);
-		
 		$model = new GtkTreeStore(GObject::TYPE_OBJECT, GObject::TYPE_STRING);
 		$iter = $model->append(NULL, [$pixbuf, "Line 1"]);
 			$iter2 = $model->append($iter, [$pixbuf, "Line 1 of 1"]);
 				$model->append($iter2, [$pixbuf, "Line 1 of 2"]);
 			$model->append($iter, [$pixbuf, "Line 2 of 1"]);
 		$tree->set_model($model);
-
 		$scroll = new GtkScrolledWindow();
 		$scroll->add($tree); // add_with_viewport is deprecated
 		$scroll->set_policy(GtkPolicyType::AUTOMATIC, GtkPolicyType::AUTOMATIC);
 		$paned->add1($scroll);
-
 		// Create notebook
 		$this->ntb = new GtkNotebook();
 		$this->ntb->set_tab_pos(GtkPositionType::TOP);
 		$paned->add2($this->ntb);
-
 		
 		$this->create_new_tab("GtkLabel.cpp");
 		$this->create_new_tab("GtkLabel.h");
 		$this->create_new_tab("main.cpp");
 		$this->create_new_tab("main.h");
-
 		// Toolbar
 		$tlb = new GtkToolbar();
 		$tlb_btn1 = new GtkToolButton("OK");
-		$tlb_btn1->set_icon_name("network-wired");
-		$tlb->insert($tlb_btn1);
+		$tlb_btn1->set_icon_name("gtk-close");
+		$tlb->insert($tlb_btn1, -1);
 
 		// VBox
 		$main_box = new GtkBox(GtkOrientation::VERTICAL);
@@ -61,44 +54,44 @@ class Application
 		$main_box->pack_start($paned, TRUE, TRUE);
 		
 		// $this->b->connect("clicked", [$this, "b_clicked"]);
-
 		// Create window
 		$this->win = new GtkWindow();
 		$this->win->set_default_size(800, 600);
 		$this->win->add($main_box);
-
 		// Connects
 		$this->win->connect("destroy", [$this, "GtkWindowDestroy"]);
 
 		// $this->win->set_interactive_debugging(TRUE);
-
 		// Show all
 		$this->win->show_all();
-
 
 		// $dialog = GtkMessageDialog::new_with_markup($this->win, GtkDialogFlags::MODAL, GtkMessageType::WARNING, GtkButtonsType::OK_CANCEL, "My <b>PHP-GTK3</b> tests");
 		$dialog = GtkMessageDialog::new_with_markup(NULL, GtkDialogFlags::MODAL, GtkMessageType::WARNING, GtkButtonsType::OK_CANCEL, "My <b>PHP-GTK3</b> tests");
 		$dialog->format_secondary_markup("This is a <i>GtkMessageDialog</i> test");
+
 		$a = $dialog->run();
 		$dialog->destroy();
+
+	
 	}
 
 	public function create_new_tab($label)
 	{
 		$hbox = new GtkBox(GtkOrientation::HORIZONTAL);
-		$button_close = GtkButton::new_from_icon_name("gtk-close");
-		$button_close->set_size_request(5, 5);
-		$label = new GtkLabel($label);
-		$hbox->pack_start($label, TRUE, TRUE, 10);
-		$hbox->pack_start($button_close, FALSE, FALSE);
 
+		$button_close = GtkButton::new_from_icon_name("gtk-close", GtkIconSize::BUTTON);
+		$button_close->set_size_request(5, 5);
+
+		$label = new GtkLabel($label);
+
+		$hbox->pack_start($label, TRUE, TRUE, 10);
+
+		$hbox->pack_start($button_close, FALSE, FALSE);
 		$text = new GtkTextView();
 		$scroll = new GtkScrolledWindow();
 		$scroll->add($text);
 		$scroll->set_policy(GtkPolicyType::AUTOMATIC, GtkPolicyType::AUTOMATIC);
-
-		$this->ntb->insert_page($scroll, $hbox);
-
+		$this->ntb->insert_page($scroll, $hbox, -1);
 		$button_close->connect("clicked", function() {
 
 			// $dialog = GtkDialog::new_with_buttons("Titulo", $this->win, GtkDialogFlags::MODAL);
@@ -170,9 +163,7 @@ class Application
 			$dialog->destroy();
 
 		});
-
 		$button_close->connect("clicked", [$this, "close_tab"], $hbox);
-
 		$hbox->show_all();
 	}
 
