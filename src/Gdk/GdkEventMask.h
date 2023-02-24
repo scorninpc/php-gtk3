7842b82f3dc6d6348e62c40f60fb4a4fdae1dfba
Sven Pohl <pohl@apsservices.de>
2023-02-24 15:45:33 +0100

    feat: event mask inital

---
+++ src/Gdk/GdkEventMask.h
@@ -0,0 +1,18 @@
+
+#ifndef _PHPGTK_GdkEVENTMASK_H_
+#define _PHPGTK_GdkEVENTMASK_H_
+
+	#include <phpcpp.h>
+	#include <gtk/gtk.h>
+
+	class GdkEventMask_ : public Php::Base
+	{
+		public:
+		    /**
+             *  C++ constructor and destructor
+             */
+			GdkEventMask_();
+
+	};
+
+#endif
