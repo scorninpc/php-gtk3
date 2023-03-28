66604fd55c816a5322a4f7f4677b2d79874f0e3d
Sven Pohl <pohl@apsservices.de>
2023-03-28 06:35:15 +0200

    feat: set_propagate_natural_height

--- src/Gtk/GtkScrolledWindow.cpp
+++ src/Gtk/GtkScrolledWindow.cpp
@@ -16,7 +16,7 @@ GtkScrolledWindow_::~GtkScrolledWindow_() = default;
  */
 void GtkScrolledWindow_::__construct(Php::Parameters &parameters)
 {
-	instance = (gpointer *)gtk_scrolled_window_new(NULL, NULL);
+    instance = (gpointer *)gtk_scrolled_window_new(NULL, NULL);
 }
 
 /**
@@ -37,31 +37,34 @@ void GtkScrolledWindow_::set_policy(Php::Parameters &parameters)
 
 void GtkScrolledWindow_::set_shadow_type(Php::Parameters &parameters)
 {
-	int int_type = (int)parameters[0];
-	GtkShadowType type = (GtkShadowType)int_type;
+    int int_type = (int)parameters[0];
+    GtkShadowType type = (GtkShadowType)int_type;
 
     gtk_scrolled_window_set_shadow_type(GTK_SCROLLED_WINDOW(instance), type);
-
 }
 
-void GtkScrolledWindow_::set_propagate_natural_height(Php::Parameters& parameters)
+void GtkScrolledWindow_::set_propagate_natural_height(Php::Parameters &parameters)
 {
     gboolean natural_height = (gboolean)parameters[0];
 
     gtk_scrolled_window_set_propagate_natural_height(GTK_SCROLLED_WINDOW(instance), natural_height);
-
 }
 
+void GtkScrolledWindow_::set_propagate_natural_width(Php::Parameters &parameters)
+{
+    gboolean natural_height = (gboolean)parameters[0];
 
-void GtkScrolledWindow_::set_max_content_height(Php::Parameters& parameters)
+    gtk_scrolled_window_set_propagate_natural_width(GTK_SCROLLED_WINDOW(instance), natural_height);
+}
+
+void GtkScrolledWindow_::set_max_content_height(Php::Parameters &parameters)
 {
     int height = (int)parameters[0];
 
     gtk_scrolled_window_set_max_content_height(GTK_SCROLLED_WINDOW(instance), height);
 }
 
-
-void GtkScrolledWindow_::set_min_content_height(Php::Parameters& parameters)
+void GtkScrolledWindow_::set_min_content_height(Php::Parameters &parameters)
 {
     int height = (int)parameters[0];
 
--- src/Gtk/GtkScrolledWindow.h
+++ src/Gtk/GtkScrolledWindow.h
@@ -2,48 +2,49 @@
 #ifndef _PHPGTK_GTKSCROLLEDWINDOW_H_
 #define _PHPGTK_GTKSCROLLEDWINDOW_H_
 
-    #include <phpcpp.h>
-    #include <gtk/gtk.h>
+#include <phpcpp.h>
+#include <gtk/gtk.h>
 
-    #include "GtkContainer.h"
+#include "GtkContainer.h"
+
+/**
+ * GtkScrolledWindow_
+ *
+ * https://developer.gnome.org/gtk3/stable/GtkScrolledWindow.html
+ */
+class GtkScrolledWindow_ : public GtkContainer_
+{
+    /**
+     * Publics
+     */
+public:
+    /**
+     *  C++ constructor and destructor
+     */
+    GtkScrolledWindow_();
+    ~GtkScrolledWindow_();
 
     /**
-     * GtkScrolledWindow_
-     * 
-     * https://developer.gnome.org/gtk3/stable/GtkScrolledWindow.html
+     * PHP Construct
      */
-    class GtkScrolledWindow_ : public GtkContainer_
-    {
-        /**
-         * Publics
-         */
-        public:
+    void __construct(Php::Parameters &parameters);
 
-            /**
-             *  C++ constructor and destructor
-             */
-            GtkScrolledWindow_();
-            ~GtkScrolledWindow_();
+    /**
+     * Sets the scrollbar policy for the horizontal and vertical scrollbars.
+     *
+     * https://developer.gnome.org/gtk3/stable/GtkScrolledWindow.html#gtk-scrolled-window-set-policy
+     */
+    void set_policy(Php::Parameters &parameters);
 
-            /**
-             * PHP Construct
-             */
-            void __construct(Php::Parameters &parameters);
+    void set_shadow_type(Php::Parameters &parameters);
 
-            /**
-             * Sets the scrollbar policy for the horizontal and vertical scrollbars.
-             *
-             * https://developer.gnome.org/gtk3/stable/GtkScrolledWindow.html#gtk-scrolled-window-set-policy
-             */
-            void set_policy(Php::Parameters &parameters);
-            
-            void set_shadow_type(Php::Parameters &parameters);
+    void set_propagate_natural_height(Php::Parameters &parameters);
 
-            void set_propagate_natural_height(Php::Parameters& parameters);
+    void set_propagate_natural_width(Php::Parameters &parameters);
 
-            void set_max_content_height(Php::Parameters& parameters);
+    void set_max_content_height(Php::Parameters &parameters);
 
-            void set_min_content_height(Php::Parameters& parameters);
-    };
+    void set_min_content_height(Php::Parameters &parameters);
+};
 
 #endif
