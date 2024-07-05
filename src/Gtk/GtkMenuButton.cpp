e01ff04f687b199ed1ff226f4d5f6bd441b79d5d
d47081 <d47081@github>
2024-07-06 02:08:38 +0300

    remove extra line

--- src/Gtk/GtkMenuButton.cpp
+++ src/Gtk/GtkMenuButton.cpp
@@ -18,11 +18,9 @@ void GtkMenuButton_::__construct()
 
 void GtkMenuButton_::set_popup(Php::Parameters& parameters)
 {
-	GtkWidget *menu;
-
 	Php::Value object_menu = parameters[0];
 	GtkWidget_ *phpgtk_menu = (GtkWidget_ *)object_menu.implementation();
-	menu = GTK_WIDGET(phpgtk_menu->get_instance());
+	GtkWidget *menu = GTK_WIDGET(phpgtk_menu->get_instance());
 
 	gtk_menu_button_set_popup (GTK_MENU_BUTTON(instance), menu);
 }
