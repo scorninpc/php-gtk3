ebc1af752eb54b9e6cf7f9977a5cf7257de3a48e
d47081 <d47081@github>
2024-07-28 02:32:40 +0300

    enshort namespaces

--- src/Pango/PangoLayout.cpp
+++ src/Pango/PangoLayout.cpp
@@ -76,24 +76,24 @@ Php::Value PangoLayout_::get_extents()
 		&logical_rect
 	);
 
-	Php::Array ret_ink_rect_arr;
+	Php::Array ink_rect_arr;
 
-		ret_ink_rect_arr["x"] = (int) ink_rect.x;
-		ret_ink_rect_arr["y"] = (int) ink_rect.y;
-		ret_ink_rect_arr["width"] = (int) ink_rect.width;
-		ret_ink_rect_arr["height"] = (int) ink_rect.height;
+		ink_rect_arr["x"] = (int) ink_rect.x;
+		ink_rect_arr["y"] = (int) ink_rect.y;
+		ink_rect_arr["width"] = (int) ink_rect.width;
+		ink_rect_arr["height"] = (int) ink_rect.height;
 
-	Php::Array ret_logical_rect_arr;
+	Php::Array logical_rect_arr;
 
-		ret_logical_rect_arr["x"] = (int) logical_rect.x;
-		ret_logical_rect_arr["y"] = (int) logical_rect.y;
-		ret_logical_rect_arr["width"] = (int) logical_rect.width;
-		ret_logical_rect_arr["height"] = (int) logical_rect.height;
+		logical_rect_arr["x"] = (int) logical_rect.x;
+		logical_rect_arr["y"] = (int) logical_rect.y;
+		logical_rect_arr["width"] = (int) logical_rect.width;
+		logical_rect_arr["height"] = (int) logical_rect.height;
 
 	Php::Array ret_arr;
 
-		ret_arr["ink_rect"] = ret_ink_rect_arr;
-		ret_arr["logical_rect"] = ret_logical_rect_arr;
+		ret_arr["ink_rect"] = ink_rect_arr;
+		ret_arr["logical_rect"] = logical_rect_arr;
 
 	return ret_arr;
 }
